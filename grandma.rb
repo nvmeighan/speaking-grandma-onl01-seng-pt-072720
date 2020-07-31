46261a71d9b6ffe817868e1ea84c95855e041b67
smiling-modem-1057 <nicole.v.meighan@gmail.com>
2020-07-31 20:36:49 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -3,7 +3,7 @@ def speak_to_grandma(phrase)
 if phrase == "I LOVE YOU GRANDMA!"
  return "I LOVE YOU TOO PUMPKIN!"
 elsif phrase == phrase.upcase
- #puts "NO, NOT SINCE 1938!"
+ return "NO, NOT SINCE 1938!"
 #else 'I LOVE YOU GRANDMA!'
  #puts "I LOVE YOU TOO PUMPKIN!"
 end
