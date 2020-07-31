ef4facac508fc664aaa1d1f1e09930b9b5bbb43f
smiling-modem-1057 <nicole.v.meighan@gmail.com>
2020-07-31 20:27:18 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,7 +1,7 @@
 def speak_to_grandma(phrase)
 
 if phrase == "I LOVE YOU GRANDMA!"
- puts "HUH?! SPEAK UP, SONNY!"
+ puts "I LOVE YOU TOO PUMPKIN!"
 #elsif == speak_to_grandma
  #puts "NO, NOT SINCE 1938!"
 #else 'I LOVE YOU GRANDMA!'
